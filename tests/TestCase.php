549e1026ef3bcc7cfb3bb556620b998c8347d49b
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-02-25 12:45:45 +0800

    Fixed old migration
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

---
+++ tests/TestCase.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Lloricode\LaravelHtmlTable\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Schema\Blueprint;
+use Lloricode\LaravelHtmlTable\Tests\Models\User;
+
+class TestCase extends Orchestra
+{
+    
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->setUpDatabase($this->app);
+    }
+
+            /**
+     * Set up the database.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function setUpDatabase($app)
+    {
+        $app['db']->connection()->getSchemaBuilder()->create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('slug');
+            $table->string('password');
+
+        });
+
+        User::create([
+            'name' => 'Orchestra',
+            'email' => 'hello@orchestraplatform.com',
+            'slug' => str_slug('Orchestra orchestraplatform'),
+            'password' => \Hash::make('456'),
+        ]);
+        User::create([
+            'name' => 'Lloric',
+            'email' => 'lloricode@gmail.com',
+            'slug' => str_slug('Lloric Garcia'),
+            'password' => \Hash::make('1234'),
+        ]);
+    }
+
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Table' => "Lloricode\\LaravelHtmlTable\\Facades\\LaravelHtmlTableFacade"
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return ['Lloricode\LaravelHtmlTable\Providers\LaravelHtmlTableProvider'];
+    }
+}
